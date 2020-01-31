8df956f2460733b9a6229297e4e78cf37048c130
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-01-30 22:50:08 -0500

    Add files via upload

---
+++ remove-elements-from-an-array-using-slice-instead-of-splice.js
@@ -0,0 +1,8 @@
+function nonMutatingSplice(cities) {
+  // Add your code below this line
+  let res = cities.slice(0,3);
+  return(res);
+  // Add your code above this line
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
