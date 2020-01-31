ea13e22bf1872786f21e0f3988e408aeb50a9e8e
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-01-30 22:50:55 -0500

    Add files via upload

---
+++ combine-two-arrays-using-the-concat-method.js
@@ -0,0 +1,10 @@
+function nonMutatingConcat(original, attach) {
+  // Add your code below this line
+let res = original.concat(attach);
+return res;
+
+  // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
