72a8fbbd67b5adfb9db7574c77c2f5bfc4c8c8a7
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-02-24 23:57:03 -0500

    Add files via upload

---
+++ missing-letters.js
@@ -0,0 +1,8 @@
+function fearNotLetter(str) {
+let res = "";
+for (let x = 0; x < str.length-1; x++)
+  if(str.charCodeAt(x)+1 != (str.charCodeAt(x+1)))
+  return(String.fromCharCode(str.charCodeAt(x)+1));
+}
+
+fearNotLetter("abce");
